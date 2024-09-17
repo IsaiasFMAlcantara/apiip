6f0fc877923c4e6ec555bf9b9ff8084434af2074
Isaias Félix <152193847+desenvolvimento1novalar@users.noreply.github.com>
2024-09-17 08:19:54 -0400

    subindo atualização do cors

--- main.py
+++ main.py
@@ -8,20 +8,17 @@ from ipv4binary import calculate_ipv4_binary
 
 app = FastAPI()
 
-# Defina as origens permitidas. 
-# Exemplo: você pode permitir todas as origens com ["*"], ou definir domínios específicos.
 origins = [
-    "http://localhost:8000",  # Origem local para desenvolvimento
-    "https://calcip-bn6s2x.flutterflow.app/",  # Substitua pelo domínio do seu front-end
-    # Adicione mais origens conforme necessário
+    "http://localhost:8000",  # desenvolvimento
+    "https://calcip-bn6s2x.flutterflow.app/",  # front-end
 ]
 
 # Adiciona o middleware CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,  # Domínios que podem acessar a API
-    allow_credentials=True,  # Permitir envio de cookies e credenciais
-    allow_methods=["*"],  # Permitir todos os métodos HTTP (GET, POST, etc.)
+    allow_credentials=True, # Permitir envio de cookies e credenciais
+    allow_methods=["POST"],  # Permitir todos os métodos HTTP (GET, POST, etc.)
     allow_headers=["*"],  # Permitir todos os cabeçalhos HTTP
 )
 
